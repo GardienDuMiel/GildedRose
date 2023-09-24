b6ec2affa64451e4c8569404b121b1f2a0f4fae6
Benoît <benoit.antunes.ba@gmail.com>
2023-09-24 19:23:56 +0200

    Sépartion du if et du else

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,7 +20,9 @@ class GildedRose {
             item.quality = item.quality - 1;
           }
         }
-      } else {
+      } 
+
+      if (!(!item.name.equals(BRIE) && !item.name.equals(PASS))) {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
 
