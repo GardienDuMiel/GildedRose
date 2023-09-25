263975cd42b4c3980de9a6e05d38a14172c6e65e
Benoît <benoit.antunes.ba@gmail.com>
2023-09-25 07:59:29 +0200

    fusion des conditions if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -50,11 +50,9 @@ class GildedRose {
 
       if (item.sellIn < 0) {
         if (!item.name.equals(BRIE)) {
-          if (!item.name.equals(PASS)) {
-            if (!item.name.equals(SULFURAS)) {
-              if (item.quality > 0) {
-                item.quality = item.quality - 1;
-              }
+          if (!item.name.equals(PASS) && !item.name.equals(SULFURAS)) {
+            if (item.quality > 0) {
+              item.quality = item.quality - 1;
             }
           } 
 
