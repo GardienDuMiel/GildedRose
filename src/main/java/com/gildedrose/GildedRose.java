18ead8f4fa8eec9cb9849a2bde41caf5aea47699
Benoît <benoit.antunes.ba@gmail.com>
2023-09-25 09:05:45 +0200

    regroupement pour 'normalItem'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -51,21 +51,15 @@ class GildedRose {
 
       if (normalItem(item)) {
         item.sellIn = item.sellIn - 1;
-      }
-      
-      if (normalItem(item)) {
-        if (item.sellIn < 0) {
-          if (item.quality > 0) {
+
+        if (item.quality > 0) {
+          item.quality = item.quality - 1;
+
+          if (item.sellIn < 0) {
             item.quality = item.quality - 1;
           }
         }
       }
-
-      if (normalItem(item)) {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1;
-        }
-      } 
       
     }
   }
