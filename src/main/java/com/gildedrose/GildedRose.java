030126637381b64d9dfd51fad235f52dcf19e43c
Benoît <benoit.antunes.ba@gmail.com>
2023-09-25 09:01:48 +0200

    regroupement pour 'backstage pass'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -27,29 +27,26 @@ class GildedRose {
       if (item.name.equals(PASS)) {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
+        }
+        
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
         }
-      }
 
-      if (item.name.equals(PASS)) {
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+
         item.sellIn = item.sellIn - 1;
-      }
 
-      if (item.name.equals(PASS)) {
         if (item.sellIn < 0) {
           item.quality = 0;
         }
+
       }
 
       if (normalItem(item)) {
