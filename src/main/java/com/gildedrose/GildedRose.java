f2cbcc9c9e731ea9d414af563cb72135989b8346
Benoît <benoit.antunes.ba@gmail.com>
2023-09-25 08:51:42 +0200

    regroupement pour 'aged brie'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,17 +20,7 @@ class GildedRose {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
         }
-      }
 
-      if (item.name.equals(BRIE)) {
-        if (item.sellIn < 0) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-
-      if (item.name.equals(BRIE)) {
         item.sellIn = item.sellIn - 1;
       }
 
